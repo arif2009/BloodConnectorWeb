3cdeb7d2347d9a7d9dcced5a0566da36ae1b9b6b
Arif <arif.rahman2009@gmail.com>
2017-08-04 10:12:58 +0600

    Register account controller

--- BloodConnector.WebAPI/App_Start/UnityConfig.cs
+++ BloodConnector.WebAPI/App_Start/UnityConfig.cs
@@ -1,6 +1,10 @@
 using System;
+using BloodConnector.WebAPI.Controllers;
 using BloodConnector.WebAPI.Interface;
+using BloodConnector.WebAPI.Models;
 using BloodConnector.WebAPI.Repository;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataHandler;
 using Microsoft.Practices.Unity;
 
 namespace BloodConnector.WebAPI.App_Start
@@ -40,6 +44,7 @@ namespace BloodConnector.WebAPI.App_Start
             // container.RegisterType<IProductRepository, ProductRepository>();
             container.RegisterType(typeof(IRepository<>), typeof(BaseRepository<>));
             container.RegisterType<IBloodGroupRepository, BloodGroupRepository>();
+            container.RegisterType<AccountController>(new InjectionConstructor());
         }
     }
 }
