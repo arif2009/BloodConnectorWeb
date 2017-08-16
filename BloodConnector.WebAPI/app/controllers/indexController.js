f6143eeddeb9450dec4d6c5da482773d4db8933c
Arif <arif.rahman2009@gmail.com>
2017-08-16 09:17:41 +0600

    Added selected menu

--- BloodConnector.WebAPI/app/controllers/indexController.js
+++ BloodConnector.WebAPI/app/controllers/indexController.js
@@ -1,7 +1,23 @@
 ﻿'use strict';
-app.controller('indexController', ['$location', 'authService', function ($location, authService) {
+app.controller('indexController', ['$scope', '$location', 'authService', function ($scope, $location, authService) {
 
     var vm = this;
+    vm.$onInit = function () {
+        vm.active = {
+            "home": true,
+            "welcome": false,
+            "user": false,
+            "logout": false,
+            "login": false,
+            "signup":false
+        };
+    };
+
+    vm.changeSelectedClass = function (menu) {
+        Object.keys(vm.active).forEach(function (key) {
+            vm.active[key] = key === menu;
+        });
+    };
 
     vm.logOut = function () {
         authService.logOut();
