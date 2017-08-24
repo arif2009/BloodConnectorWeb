a7dfa3cf006c16477971902135e321c3a1974bec
Arif <arif.rahman2009@gmail.com>
2017-08-25 02:20:41 +0600

    Added android project

---
+++ BloodConnector.Android/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace BloodConnector.Android
+{
+    [Activity(Label = "BloodConnector", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+             SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
