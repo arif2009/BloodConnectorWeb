f29bcdb9bf748d7b134124b89638f738b392f4a2
Arif <arif.rahman2009@gmail.com>
2017-09-07 19:09:29 +0600

    Added logger class

---
+++ BloodConnector.Shared/Log/Logger.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodConnector.Shared.Log
+{
+    public static class Logger
+    {
+
+        private static void LogWrite(string message)
+        {
+            var exception = new Exception(message);
+            Elmah.ErrorLog.GetDefault(HttpContext.Current).Log(new Elmah.Error(exception));
+        }
+
+        public static void Log(string message)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine();
+            sb.AppendLine();
+            sb.AppendFormat("===============Event Occuurred at {0}===================", DateTime.Now.ToShortTimeString());
+            sb.AppendLine();
+            sb.AppendLine();
+            sb.Append(message);
+            sb.AppendLine();
+            sb.AppendLine();
+            sb.AppendLine("=======================================================");
+            Logger.LogWrite(sb.ToString());
+        }
+
+        public static void Log(Exception ex)
+        {
+            try
+            {
+                Elmah.ErrorLog.GetDefault(HttpContext.Current).Log(new Elmah.Error(ex));
+            }
+            catch (Exception x)
+            {
+                //continue
+            }
+        }
+    }
+}
