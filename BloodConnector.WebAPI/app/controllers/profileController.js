71992ab536809bcef49fd62fb2e8a1ffb42ec8c5
Arif <arif.rahman2009@gmail.com>
2017-10-13 10:48:42 +0600

    Update profile controller

--- BloodConnector.WebAPI/app/controllers/profileController.js
+++ BloodConnector.WebAPI/app/controllers/profileController.js
@@ -1,4 +1,30 @@
 ﻿'use strict';
-app.controller('profileController', [function () {
+app.controller('profileController', ['dataService', function (dataService) {
     var vm = this;
+    vm.savedSuccessfully = false;
+    vm.editMode = false;
+    vm.messages = [];
+    vm.bloodGrups = [];
+    vm.registration = {
+        firstName: "",
+        lastName: "",
+        nikeName: "",
+        email: "",
+        phoneNumber: "",
+        bloodGroupId: ""
+    };
+
+    vm.toggleEditMode = function() {
+        vm.editMode = !vm.editMode;
+    };
+
+    vm.disablEditeMode = function() {
+        vm.editMode = false;
+    };
+
+    vm.populateBloodGroup = function () {
+        dataService.getBloodGroup.then(function (result) {
+            vm.bloodGroups = result.data;
+        });
+    };
 }]);
